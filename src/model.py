3a987ae99eafb67049fb970bcd0eac296e257dea
devyn-miller <144622580+devyn-miller@users.noreply.github.com>
2024-04-08 09:51:50 -0700

    implementing tuner

--- src/model.py
+++ src/model.py
@@ -2,11 +2,52 @@ import tensorflow as tf
 from tensorflow.keras.layers import Input, Conv2D, BatchNormalization, MaxPool2D, Flatten, Dense
 import matplotlib.pyplot as plt
 from dataloader_eda import data_generator, display_image
+from kerastuner import HyperModel
+from kerastuner.tuners import RandomSearch
 
-def modeling():
-    input_ = Input(shape=[380, 676, 3], name='image')
+class MyHyperModel(HyperModel):
+    def __init__(self, input_shape):
+        self.input_shape = input_shape
 
-    x = input_
+    def build(self, hp):
+        input_ = Input(shape=self.input_shape, name='image')
+        x = input_
+        for i in range(hp.Int('conv_blocks', 3, 5, default=3)):
+            filters = hp.Int('filters_' + str(i), 32, 256, step=32)
+            for _ in range(2):
+                x = Conv2D(filters, kernel_size=(3, 3), padding='same')(x)
+                x = BatchNormalization()(x)
+                x = Activation('relu')(x)
+            if i < hp.get('conv_blocks') - 1:
+                x = MaxPooling2D(pool_size=(2, 2))(x)
+        x = GlobalAveragePooling2D()(x)
+        x = Dense(hp.Int('dense_units', 32, 128, step=32), activation='relu')(x)
+        output = Dense(4, activation='linear', name='coords')(x)
+        model = tf.keras.Model(inputs=input_, outputs=output)
+
+        model.compile(optimizer=tf.keras.optimizers.Adam(hp.Choice('learning_rate', [1e-2, 1e-3, 1e-4])),
+                      loss='mse',
+                      metrics=['accuracy'])
+        return model
+
+def tune_model():
+    hypermodel = MyHyperModel(input_shape=[380, 676, 3])
+
+    tuner = RandomSearch(
+        hypermodel,
+        objective='val_accuracy',
+        max_trials=10,
+        executions_per_trial=2,
+        directory='tuner_results',
+        project_name='car_object_detection'
+    )
+
+    tuner.search_space_summary()
+
+    tuner.search(data_generator(), epochs=10, validation_split=0.2)
+
+    best_model = tuner.get_best_models(num_models=1)[0]
+    best_model.save('best_model.h5')
 
     for i in range(10):
         n_filters = 2**(i + 3)
--- src/train.py
+++ src/train.py
@@ -3,6 +3,8 @@ from model import model
 from dataloader_eda import data_generator
 from model import ShowTestImages
 import json
+from kerastuner.tuners import RandomSearch
+from src.model import MyHyperModel
 
 def train_model():
     with tf.device('/GPU:0'):
@@ -18,4 +20,24 @@ def train_model():
     with open('model_history.json', 'w') as file:
         json.dump(history.history, file)
 
-# model.save('car-object-detection.h5')
+model.save('car-object-detection.h5')
+
+def tune_model():
+    hypermodel = MyHyperModel(input_shape=[380, 676, 3])
+
+    tuner = RandomSearch(
+        hypermodel,
+        objective='val_accuracy',
+        max_trials=10,
+        executions_per_trial=2,
+        directory='tuner_results',
+        project_name='car_object_detection'
+    )
+
+    tuner.search_space_summary()
+
+    tuner.search(data_generator(), epochs=10, validation_split=0.2)
+
+    best_models = tuner.get_best_models(num_models=5)
+    for i, model in enumerate(best_models):
+        model.save(f'best_model_{i}.h5')
